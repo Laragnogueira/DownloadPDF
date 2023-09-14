f170184c93821f25e9938a853a0f5722b077ae97
Lara Nogueira <laranogueiragabi@gmail.com>
2023-09-14 18:27:07 -0300

    salvar o download na memoria do dispositivo

---
+++ README.md
@@ -0,0 +1,12 @@
+# DownloadPDF 
+
+**Baixe arquivos em PDF! Copie o link do arquivo que deseja baixar e pronto! E ainda tem a opção de visualizar o arquivo que foi baixado.**
+
+## Biblioteca utilizada:
+> implementation 'com.itextpdf:itextg:5.5.10'
+
+##
+<picture>
+  <img alt="app" src="https://github.com/Laragnogueira/DownloadPDF/assets/102356793/97da0d21-6e6a-4d7f-8713-d0acd7901a01">
+</picture>
+
---
+++ app/src/main/java/com/example/gerarpdf/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.gerarpdf
+
+import android.app.DownloadManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gerarpdf.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonDownload.setOnClickListener {
+            val url = binding.editTextUrl.text.toString()
+            val request = DownloadManager.Request(Uri.parse(url))
+                .setTitle("document")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setAllowedOverMetered(true)
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "document.pdf")
+
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+        }
+
+        binding.buttonOpenDownload.setOnClickListener {
+            val url = binding.editTextUrl.text.toString()
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
+        }
+    }
+}
